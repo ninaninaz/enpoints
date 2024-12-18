8d84033685b8e0376d3113e5de18ded0a31c3b4b
nina.z@live.se <nina.z@live.se>
2024-12-18 12:46:43 +0100

    connected to DB

--- src/service.js
+++ src/service.js
@@ -1,8 +1,11 @@
-require('dotenv').config();
+require("dotenv").config({ path: "../.env" })
 const app = require('./app.js');
 const path = require('path');
+const connectDB = require("./config/db")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
+
+connectDB()
 
 app.listen(PORT, () => {
 	console.log(`http server listening on port: ${PORT}`)
