75bc7f85e59a76baba0f162e56c9a2e8b8001e95
Emilia Mailian <emiliamailian@gmail.com>
2024-12-19 10:47:04 +0100

    added routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const userRouter = require('./routes/user_routes');
 const app = express();
+
 app.use(express.json());
+app.use("/api/users",userRouter);
 
 module.exports = app;
