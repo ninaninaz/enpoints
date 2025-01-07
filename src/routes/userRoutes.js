43357ed2688bea38876a6af6d86364e6e225d351
aggeB8 <brannmarkdev@gmail.com>
2025-01-07 11:40:48 +0100

    added register user endpoint in userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const router = express.Router();
+import userController from "../controllers/userController"
 
-router.get("/", (req, res) => {
-	res.send({success:"true"});
-})
+const express = require("express")
+const router = express.Router()
 
+// router.get("/", (req, res) => {
+// 	res.send({success:"true"});
+// })
 
-router.post("/", (req, res) => {
-        res.send({success:"true"});
-})
+router.post("/", userController.registerUser)
 
+// router.put("/", (req, res) => {
+//         res.send({success:"true"});
+// })
 
-router.put("/", (req, res) => {
-        res.send({success:"true"});
-})
+// router.delete("/", (req, res) => {
+//         res.send({success:"true"});
+// })
 
-
-router.delete("/", (req, res) => {
-        res.send({success:"true"});
-})
-
-module.exports = router; 
+module.exports = router
